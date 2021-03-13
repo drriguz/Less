3ce3072d4743c6048d499c6339fe344867ea9c39
Riguz Lee <solee.linux@gmail.com>
2021-03-13 20:32:57 +0800

    added navigation bar

--- Less.template.php
+++ Less.template.php
@@ -1,44 +1,58 @@
 <?php
 
-class LessTemplate extends BaseTemplate {
-	public function execute() {
-		$this->html( 'headelement' ); ?>
+class LessTemplate extends BaseTemplate
+{
+	public function execute()
+	{
+		$this->html('headelement'); ?>
 
-<!-- START LESS TEMPLATE -->
+		<!-- START LESS TEMPLATE -->
 
 
-<div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
-  <button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
-  <div class="title-bar-title">Menu1</div>
-</div>
+		<div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
+			<button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
+			<div class="title-bar-title"><?php echo $this->text('sitename'); ?></div>
+		</div>
 
-<div class="top-bar" id="responsive-menu" data-animate="hinge-in-from-top spin-out">
-  <div class="top-bar-left">
-    <ul class="dropdown menu" data-dropdown-menu>
-      <li class="menu-text">Site Title</li>
-      <li class="has-submenu">
-        <a href="#0">One</a>
-        <ul class="submenu menu vertical" data-submenu>
-          <li><a href="#0">One</a></li>
-          <li><a href="#0">Two</a></li>
-          <li><a href="#0">Three</a></li>
-        </ul>
-      </li>
-      <li><a href="#0">Two</a></li>
-      <li><a href="#0">Three</a></li>
-    </ul>
-  </div>
-  <div class="top-bar-right">
-    <ul class="menu">
-      <li><input type="search" placeholder="Search"></li>
-      <li><button type="button" class="button">Search</button></li>
-    </ul>
-  </div>
-</div>
 
-<!-- END LESS TEMPLATE   -->
-<?php $this->printTrail(); ?>
-</body>
-</html><?php
-	}
-}
+		<div class="top-bar" id="responsive-menu" data-animate="hinge-in-from-top spin-out">
+			<div class="top-bar-left">
+				<ul class="dropdown menu" data-dropdown-menu>
+					<li>
+						<a href="<?php echo $this->data['nav_urls']['mainpage']['href']; ?>">
+							<img alt="<?php echo $this->text('sitename'); ?>" class="top-bar-logo" src="<?php echo $this->text('logopath') ?>">
+						</a>
+					</li>
+					<?php foreach ($this->getSidebar() as $boxName => $box) {
+						if (($box['header'] != wfMessage('toolbox')->text())) { ?>
+							<li id='<?php echo Sanitizer::escapeIdForAttribute($box['id']) ?>' <?php echo Linker::tooltip($box['id']) ?>>
+								<a href="#"><?php echo htmlspecialchars($box['header']); ?></a>
+								<?php if (is_array($box['content'])) { ?>
+									<ul class="submenu menu vertical" data-submenu>
+										<?php foreach ($box['content'] as $key => $item) {
+											echo $this->makeListItem($key, $item);
+										} ?>
+									</ul>
+								<?php } ?>
+							</li>
+					<?php }
+					} ?>
+				</ul>
+			</div>
+			<div class="top-bar-right">
+				<form action="<?php $this->text('wgScript'); ?>" id="searchform" class="mw-search">
+					<ul class="menu">
+						<li><?php echo $this->makeSearchInput(array('placeholder' => wfMessage('searchsuggest-search')->text(), 'id' => 'searchInput')); ?></li>
+						<li><button type="submit" class="button search"><?php echo wfMessage('search')->text() ?></button></li>
+					</ul>
+				</form>
+			</div>
+		</div>
+
+		<!-- END LESS TEMPLATE   -->
+		<?php $this->printTrail(); ?>
+		</body>
+
+		</html><?php
+			}
+		}
